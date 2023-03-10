1ed0d338f75a40b2431c8aa15137ef89371b3cb8
Matheus Lacerda <matheuslcd@hotmail.com>
2023-03-10 18:02:24 -0300

    create inital migrates

--- routes/web.php
+++ routes/web.php
@@ -17,12 +17,3 @@ use App\Http\Controllers\EventController;
 
 Route::get('/', [EventController::class, 'index']);
 Route::get('/events/create', [EventController::class, 'create']);
-
-Route::get('/produtos', function () {
-    $busca = request('search');
-    return view('products');
-});
-
-Route::get('/produtos_teste/{id?}', function ($id = null) {
-    return view('product', ['id' => $id]);
-});
