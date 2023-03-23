c6c409f0786301e6bbc32979fb76ec9a69800fd8
Matheus Lacerda <matheuslcd@hotmail.com>
2023-03-23 17:05:18 -0300

    adding events options

--- resources/views/events/create.blade.php
+++ resources/views/events/create.blade.php
@@ -1,8 +1,39 @@
 @extends('layouts.main')
 
-@section('title', 'Produto')
+@section('title', 'Criar evento')
 
 @section('content')
-<h1>Crie um evento</h1>
+
+<div id="event-create-container" class="col-md-6 offset-md-3">
+    <h1>Crie o seu evento</h1>
+    <form action="/events" method="POST" enctype="multipart/form-data">
+        @csrf
+        <div class="form-group">
+            <label for="image">Imagem do Evento:</label>
+            <input type="file" class="form-control-file" id="image" name="image">
+        </div>
+        <div class="form-group">
+            <label for="title">Evento:</label>
+            <input type="text" class="form-control" id="title" name="title" placeholder="Nome do evento">
+        </div>
+        <div class="form-group">
+            <label for="city">Cidade:</label>
+            <input type="text" class="form-control" id="city" name="city" placeholder="Local do evento">
+        </div>
+        <div class="form-group">
+            <label for="private">O evento é privado?</label>
+            <select type="text" class="form-control" id="private" name="private" placeholder="Nome do evento">
+                <option value="0">Sim</option>
+                <option value="1">Não</option>
+            </select>
+        </div>
+        <div class="form-group">
+            <label for="description">Evento:</label>
+            <textarea class="form-control" id="description" name="description"
+                placeholder="O que vai acontecer no evento"></textarea>
+        </div>
+        <input type="submit" class="btn btn-primary" value="Criar evento">
+    </form>
+</div>
 
 @endsection
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -11,12 +11,16 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Roboto" rel="stylesheet">
 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous">
+    </script>
 
     <link rel="stylesheet" href="/css/styles.css">
     <script src="/js/script.js"></script>
 </head>
+
 <body>
     <header>
         <nav class="navbar navbar-expand-lg navbar-light">
@@ -42,7 +46,16 @@
         </nav>
     </header>
 
-    @yield('content')
+    <main>
+        <div class="container-fluid">
+            <div class="row">
+                @if(session('msg'))
+                <p class="msg">{{ session('msg') }}</p>
+                @endif
+                @yield('content')
+            </div>
+        </div>
+    </main>
     <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
     <footer>
         <p>HDC Event &copy; 2020</p>
